8ca01f3f319b72f14d33443d71739102993faafe
gaurishankar007 <57608187+gaurishankar007@users.noreply.github.com>
2022-02-18 09:29:14 +0545

    Solvign profile information view bug

--- src/components/profile/ProfileMain.js
+++ src/components/profile/ProfileMain.js
@@ -81,7 +81,7 @@ const ProfileMain =()=> {
                             </div>
                             <div className="d-flex align-items-center">                            
                                 <h2 className="keyPA me-3">Birthday:</h2>
-                                <h2>{profileData.birthday.split("T")[0]}</h2>
+                                <h2>{profileData.birthday===null ? "" : profileData.birthday.split("T")[0]}</h2>
                             </div>
                             <div className="d-flex align-items-center">                            
                                 <h2 className="keyPA me-3">Biography:</h2>
