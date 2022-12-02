da0178ec260b8d26fc86638d6fa5a2568bb4bafc
gaurishankar007 <gaurisharma358@gmail.com>
2022-12-02 12:49:25 +0545

    new method

--- database/db.js
+++ database/db.js
@@ -1,6 +1,15 @@
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
 
-mongoose.connect('mongodb://127.0.0.1:27017/momento', {
+mongoose
+  .connect(process.env.MONGODB, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
-})
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("MongoDB Connected on SmoothPlayer Cluster.");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
