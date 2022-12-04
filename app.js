574d6b0f37b616116739dd00ad107a79586dda04
gaurishankar007 <gaurisharma358@gmail.com>
2022-12-04 16:57:39 +0545

    socket io

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const cors = require("cors");
 app.use(cors());
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-app.use(express.static(__dirname+"/uploads"))
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname + "/uploads"));
 
 require("./database/db");
 
@@ -61,4 +61,45 @@ app.use(messageRoute);
 const dotenv = require("dotenv");
 dotenv.config();
 const port = process.env.PORT || 4040;
-app.listen(port, ()=> {console.log("Listening on port: "+port+"...")});
+const server = app.listen(port, () => {
+  console.log("Listening on port: " + port + "...");
+});
+
+const io = require("socket.io")(server, {
+  pingTimeout: 60000,
+  cors: {
+    origin: ["http://localhost:3000"],
+  },
+});
+
+io.on("connection", (socket) => {
+  socket.on("setup", (userData) => {
+    socket.join(userData._id);
+    socket.emit("connected");
+  });
+
+  socket.on("join chat", (room) => {
+    socket.join(room);
+    console.log("User Joined Room: " + room);
+  });
+
+  socket.on("typing", (room) => socket.in(room).emit("typing"));
+  socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
+
+  socket.on("new message", (newMessageReceived) => {
+    var chat = newMessageReceived.chat;
+
+    if (!chat.users) return console.log("chat.users not defined");
+
+    chat.users.forEach((user) => {
+      if (user._id == newMessageReceived.sender._id) return;
+
+      socket.in(user._id).emit("message received", newMessageReceived);
+    });
+  });
+
+  socket.off("setup", () => {
+    console.log("USER DISCONNECTED");
+    socket.leave(userData._id);
+  });
+});
